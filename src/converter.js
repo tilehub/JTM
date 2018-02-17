87cc6a4d287a54233076386bd6eee94374cb8c9f
Maximilian Müller <maximilian-mueller@gmx.at>
2018-02-17 17:47:09 +0100

    added converter.js

---
+++ src/converter.js
@@ -0,0 +1,24 @@
+
+var TMXJsonToJTM = function(tmx) {
+    //ToDo: convert TMX to JTM
+}
+
+var JTMToTMXJson = function(jtm) {
+   //ToDo: convert TMX to JTM
+}
+
+var TMXxmlToTMXJson = function(tmx) {
+   //ToDo: convert TMX to JTM
+}
+
+var TMXJsonToTMXxml = function(tmx) {
+   //ToDo: convert TMX to JTM
+}
+
+var JTMToTMXxml = function(jtm) {
+   //ToDo: convert TMX to JTM
+}
+
+var TMXxmlToJTM = function(tmx) {
+    //ToDo: convert TMX to JTM
+}
